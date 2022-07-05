d6275bb638d1c7b778fad0042126931c5733e05f
sahion <alexeysuvorov33@gmail.com>
2022-07-05 09:49:52 +0700

    feat: add dependencies of webpack, eslint, package.json

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.ts',
+  module:{
+    rules:[
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        include: [path.resolve(__dirname,'src')]
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      }
+    ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, './src/index.html'),
+        filename: 'index.html',
+    }),
+    new CleanWebpackPlugin(),
+],
+}
