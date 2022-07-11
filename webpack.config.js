b2c79c7e46689bfb68177d6ef8095cc3f7faf407
sahion <alexeysuvorov33@gmail.com>
2022-07-12 01:51:01 +0700

    feat: add prettier for project

--- webpack.config.js
+++ webpack.config.js
@@ -5,28 +5,28 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
   mode: 'production',
   entry: './src/index.ts',
-  module:{
-    rules:[
+  module: {
+    rules: [
       {
         test: /\.ts$/,
         use: 'ts-loader',
-        include: [path.resolve(__dirname,'src')]
+        include: [path.resolve(__dirname, 'src')],
       },
       {
         test: /\.s[ac]ss$/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
-      }
-    ]
+      },
+    ],
   },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
     new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, './src/index.html'),
-        filename: 'index.html',
+      template: path.resolve(__dirname, './src/index.html'),
+      filename: 'index.html',
     }),
     new CleanWebpackPlugin(),
-],
-}
+  ],
+};
