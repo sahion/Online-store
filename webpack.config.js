d447c3eae24335a3f4114f56ca49ab19c629233e
sahion <alexeysuvorov33@gmail.com>
2022-07-15 23:10:21 +0700

    feat: create style for header

--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,10 @@ module.exports = {
         test: /\.s[ac]ss$/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
   output: {
